f1eb28d36267010331fe1e469ffc3325c984383a
barsukzhigul <pain.kenin@gmail.com>
2021-04-14 03:27:31 +0300

    done media till 540px

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-// const element = document.querySelector("#footer__callback");
-// element.onclick = async()=>{
-//     console.log(await fetch("/", {method:"POST", body:JSON.stringify({})}))
-// }
+const element = document.querySelector("#footer__callback");
+element.onclick = async()=>{
+    console.log(await fetch("/", {method:"POST", body:JSON.stringify({})}))
+}
